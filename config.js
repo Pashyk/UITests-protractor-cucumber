9f8a3cc9dc2820db9bf63fca8699a79f42a10217
Pashyk <48953598+Pashyk@users.noreply.github.com>
2019-09-18 17:21:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: ['no-sandbox', '--headless],
+            args: ['no-sandbox', '--headless'],
         },
     },
     ignoreSynchronization: true,
